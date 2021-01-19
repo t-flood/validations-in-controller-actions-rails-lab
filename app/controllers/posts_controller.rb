131f8a4f0e184daa10eabe2b1f43a4de054e1657
Tim Flood <tflood9@gmail.com>
2021-01-19 10:34:34 -0500

    commit tree edcaa51a7a3e3013d25d627f4d75f9717e939f67

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,29 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
     @post.update(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
 
-    redirect_to post_path(@post)
+  def create
+    @post.new(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
